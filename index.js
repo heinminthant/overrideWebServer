bac55e7e939fdab19231d75b253bfe83394c647d
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-09 14:59:58 +0630

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(express.static('public/start.html'))
+app.use(express.static('public'))
 
 
 // Home Page
