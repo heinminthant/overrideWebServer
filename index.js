4fe6de6be5b7b2d3b9b81f82da21f80b1c8ae9c8
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-15 18:25:48 +0630

    first Commit

--- index.js
+++ index.js
@@ -30,13 +30,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/gg',(req,res) => {
-    if(req.session.page_views){
-        req.session.page_views++;
-        res.send("You visited this page " + req.session.page_views + " times");
-     } else {
-        req.session.page_views = 1;
-        res.send("Welcome to this page for the first time!");
-     }
+    req.session.id
 });
 
 
