03e85a5c1dd9f7d881d4ab6ec958dc365524a7b2
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-15 16:57:58 +0630

    first Commit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/viberGuide',(req,res)=>{
 })
 
 app.get('/trainingPhrases',(req,res)=>{
-    res.render(__dirname + '/public/trainingPhrases')
+    res.render('pages/trainingPhrases')
 })
 
 app.post('/storePhrases',(req,res)=>{
