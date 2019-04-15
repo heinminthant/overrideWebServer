37ace50d3e932071d481a01aada83dd4d592921b
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-15 17:05:30 +0630

    first Commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.set('views', './views');
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
