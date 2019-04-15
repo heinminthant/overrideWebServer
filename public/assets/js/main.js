cb931c77b9362fdb0590ad87888b4863eb72f574
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-15 15:21:52 +0630

    first Commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $("#add").click(function (e) { 
         counter++
         e.preventDefault()
-        $("#phrases").append('<div class="row" id="phrase' + counter + '"><input class="input-field col s9" type="text" placeholder="Add Training Phrase" class="validate"> <a class="waves-effect waves-light btn" id="delete"><i class="material-icons">delete</i></a></div>');
+        $("#phrases").append('<div class="row" id="phrase' + counter + '"><input class="input-field col s12" type="text" placeholder="Add Training Phrase" class="validate"> <a class="waves-effect waves-light btn" id="delete"><i class="material-icons">delete</i></a></div>');
       
     });
     $('#delete').click(function (e) { 
