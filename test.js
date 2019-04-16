9206808a8e9c675b6a05ee94195bd1016046a9e6
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-16 14:30:08 +0630

    first Commit

--- test.js
+++ test.js
@@ -1,4 +1,23 @@
 const db = require('./dbNew')
+const dialogflow = require('dialogflow')
+const crypto = require('./crypto')
+
 db.getDocument(810191770).then(function(result){
-    console.log(result)
+    var privateKey = result.chat_service.dialogflow.private_key;
+    var projectID = result.chat_service.dialogflow.project_id
+    var privateKey = crypto.decrypt(privateKey);
+      
+      let config = {
+        credentials: {
+          private_key: privateKey,
+          client_email: result.chat_service.dialogflow.client_email
+        }
+      }
+
+      const agentClient = new dialogflow.AgentsClient(config)
+
+      
+      agentClient.
+    //   agentClient.getAgent()
+    //   agentClient.trainAgent()
 })
