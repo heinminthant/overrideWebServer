92e68543cd77fb1fcf322b4e9438094e5db1796d
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-17 14:38:14 +0630

    first Commit

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ const db = require('./dbNew')
 const dialogflow = require('dialogflow')
 const crypto = require('./crypto')
 
+var num = crypto.decrypt('68a5b7db992c1807218b485b58ac3c1f')
+console.log(num)
