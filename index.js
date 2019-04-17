1cae9f3655c68cf1bf8b2d85ff423d20fca35a68
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-17 21:33:54 +0630

    first Commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const formidable = require('formidable')
+
 
 
 
@@ -346,8 +348,19 @@ app.post('/storePhrases', (req, res) => {
 })
 
 app.post('/storeResponses', (req, res) => {
-    console.log(req.body.data)
-
+    app.post('/submit-form', (req, res) => {
+        new formidable.IncomingForm().parse(req, (err, fields, files) => {
+          if (err) {
+            console.error('Error', err)
+            throw err
+          }
+          console.log('Fields', fields)
+          console.log('Files', files)
+          files.map(file => {
+            console.log(file)
+          })
+        })
+      })
     // var userID = req.body.userID
     // var intentID = req.body.intentID
     // var responses = req.body.responses
