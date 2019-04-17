ade9e3447c61afc837c6a802246f3b64711f7ce4
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-17 14:20:04 +0630

    first Commit

--- index.js
+++ index.js
@@ -91,10 +91,10 @@ async function exec(){
                        dflow.detectIntent(projectID,config,req.body.message.text).then(function(result){
                            var intentID = result.intent.name.split('/')[4]
                            db.getResponses(intentID,document.user_id).then(function(result){
-                               console.log(result.responses)
+                               console.log(result)
                             //    responses.forEach(function(response){
                             //        if(response.type === 'text'){
-                            //             viber.sendMessage(req.body.sender.id,req.body.message.text,vtoken)
+                            //             viber.sendMessage(req.body.sender.id,req.body.message.text,)
                             //        }
                             //    })
                            })
