25479aedd968dcb563d194c6ec2342ae8c84a07e
heinminthant <35863140+heinminthant@users.noreply.github.com>
2019-04-18 02:58:14 +0630

    first Commit

--- viber.js
+++ viber.js
@@ -63,8 +63,58 @@ module.exports = {
 
     },
     sendCarousel: function(receiver,token,content){
-            
+        var buttons = []
+
+        content.forEach(function(con){
+            buttons.push( {	
+                "Columns":6,
+                "Rows":4,
+                "ActionType":"none",
+                "Image":con.imgURL,
+                "BgColor":"#A12412"
+                
+             })
+             buttons.push({
+                "Columns":6,
+                "Rows":2,
+                "ActionType":"none",
+                "Text":"<font color=#323232><b>" + con.title + "</b></font><br><font color=#777777>"+ con.subtitle +"</font>",
+                "TextSize":"large",
+                "TextVAlign":"center",
+                "TextHAlign":"middle",
+               
+                "BgColor":"#A12412"
+                
+             })
+             buttons.push({
+                "Columns":6,
+                "Rows":1,
+                "ActionType":"reply",
+                "ActionBody": "Yo Yo",
+                "Text":"<font color=#323232><b>"+con.button+"</b></font>",
+                "TextSize":"large",
+                "TextVAlign":"center",
+                "TextHAlign":"middle",
+                "BgColor":"#E2211D"
+                
+             })
+
+        })
+        console.log(buttons)
+
+
+        var req = {  
+            "receiver":receiver,
+            "type":"rich_media",
+            "min_api_version":2,
+            "rich_media":{
+               "Type":"rich_media",
+               "ButtonsGroupColumns":6,
+               "ButtonsGroupRows":7,
+               "BgColor":"#FFFFFF",
+               
+         }
     }
 
-
+    }
 }
